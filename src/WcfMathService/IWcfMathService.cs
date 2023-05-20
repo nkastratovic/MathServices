71b11364ecdf7cc688726df20f517ab8022629ba
Nenad Kastratovic <nenad.kastratovic@hpe.com>
2023-05-20 20:44:06 +0200

    Created a WCF math service providing access to operations in the math library and create a client for the created service.

---
+++ src/WcfMathService/IWcfMathService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WcfMathService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IWcfMathService" in both code and config file together.
+    [ServiceContract]
+    public interface IWcfMathService
+    {
+        [OperationContract]
+        int Add(int a, int b);
+
+        [OperationContract]
+        int Subtract(int a, int b);
+
+        [OperationContract]
+        int Multiply(int a, int b);
+
+        [OperationContract]
+        double Divide(int a, int b);
+    }
+}
---
+++ src/WcfMathService/WcfMathService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WcfMathService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "WcfMathService" in both code and config file together.
+    public class WcfMathService : IWcfMathService
+    {
+        public int Add(int a, int b)
+        {
+            return MathLibrary.MathClass.Add(a, b);
+        }
+
+        public double Divide(int a, int b)
+        {
+            return MathLibrary.MathClass.Divide(a, b);
+        }
+
+        public int Multiply(int a, int b)
+        {
+            return MathLibrary.MathClass.Multiply(a, b);
+        }
+
+        public int Subtract(int a, int b)
+        {
+            return MathLibrary.MathClass.Subtract(a, b);
+        }
+    }
+}
