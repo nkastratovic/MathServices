a31c6813e4d62f210b96399e7126cbaca3d131db
Nenad Kastratovic <nenad.kastratovic@hpe.com>
2023-05-20 11:11:33 +0200

    Created a simple C# math library and a console application that makes use of this library.

---
+++ src/MathLibrary/MathClass.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathLibrary
+{
+    public class MathClass
+    {
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+
+        public static int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+
+        public static double Divide(double a, double b)
+        {
+            if (b != 0)
+                return a / b;
+            else
+                throw new DivideByZeroException("Cannot divide by zero.");
+        }
+    }
+}
---
+++ src/MathServicesConsoleApp/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathServicesConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the first number:");
+            string input1 = Console.ReadLine();
+            int a;
+
+            if (int.TryParse(input1, out a))
+            {
+                Console.WriteLine("Enter the second number:");
+                string input2 = Console.ReadLine();
+                int b;
+
+                if (int.TryParse(input2, out b))
+                {
+                    Console.WriteLine($"{a} + {b} = {MathLibrary.MathClass.Add(a, b)}");
+                    Console.WriteLine($"{a} + {b} = {MathLibrary.MathClass.Subtract(a, b)}");
+                    Console.WriteLine($"{a} + {b} = {MathLibrary.MathClass.Multiply(a, b)}");
+                    Console.WriteLine($"{a} + {b} = {MathLibrary.MathClass.Divide(a, b)}");
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input for the second number.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid input for the first number.");
+            }
+            Console.ReadKey();
+        }
+    }
+}
